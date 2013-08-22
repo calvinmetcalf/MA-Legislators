2cf68831c10bd5ee0bfa9884e18be8cd2fbc6b1a
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-22 14:30:41 -0400

    smalltopo

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ var senate={};
 senate.geo = require('./geometry/senate.json');
 senate.prop = require('./properties/senate.json');
 senate.name='senate';
-var types = [congress,house,senate];
+var types = [congress,senate,house];
 var done = 0;
 function doTopo(){
     var topo = spawn('./node_modules/topojson/bin/topojson', ['-p','-o','legislators.topojson','--','congress=congress.geojson','house=house.geojson','senate=senate.geojson']);
@@ -33,16 +33,38 @@ topo.on('close', function (code) {
     }
   
 });
+
 }
-types.forEach(function(doc){
+var convert =  function(){
+    var doc = types[done];
     doc.geo.features.forEach(function(v){
         v.properties=doc.prop[v.id];
     });
     fs.writeFile('./'+doc.name+'.geojson',JSON.stringify(doc.geo,false,4),{encoding:'utf8'},function(){
         console.log('done with',doc.name);
-        done++;
+        
+        var topo = spawn('./node_modules/topojson/bin/topojson', ['-p','-o',doc.name+'.topojson','--',doc.name+'='+doc.name+'.geojson']);
+        topo.stdout.on('data', function (data) {
+  console.log('topojson: '+data);
+});
+
+topo.stderr.on('data', function (data) {
+  console.error('topojson: '+data);
+});
+
+topo.on('close', function (code) {
+    if(code===0){
+        console.log('all done');
+    }else{
+        console.error('something bad happend, topojson exited with code '+code);
+    }
+  done++;
         if(done===types.length){
             doTopo();
+        }else{
+            convert();
         }
-    });
 });
+    });
+};
+convert();
